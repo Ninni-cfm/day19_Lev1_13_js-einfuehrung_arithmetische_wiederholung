004864863ba40e31a23b355957e65569de13f7d7
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-07-15 14:37:18 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// Übertrage folgende Rechenarten in deinen Code - Editor und schaue dir das Ergebnis an:
+
+// Verwende Grundrechenarten
+// Addition
+let additionOperator = 1 + 1;
+console.log("Addition: " + additionOperator);
+
+// Subtraktion
+let subtraktionOperator = 2 - 1;
+console.log("Subtraktion: " + subtraktionOperator);
+
+// Multiplikation
+let multiplicationOperator = 2 * 2;
+console.log("Multiplikation: " + multiplicationOperator);
+
+// Division
+let divisionOperator = 4 / 2;
+console.log("Division: " + divisionOperator);
+
+// Modulo: zeigt an, was der Rest sein würde.
+let moduloOperator = 14 % 5;
+console.log("Modulo: " + moduloOperator);
